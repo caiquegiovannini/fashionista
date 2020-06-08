b176468e60c24d409a6759996e953398b59670f4
Caique <caique_giovannini@hotmail.com>
2020-06-08 14:18:48 -0300

    add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiSearch, FiShoppingBag } from 'react-icons/fi';
+
+import './styles.css';
+
+const Header = () => {
+  return (
+      <header className="header">
+        <Link to="/">
+          <h1 className="header__logo">Fashionista</h1>
+        </Link>
+
+        <div>
+          <button className="header__button">
+            <FiSearch className="header__button-icon" />
+          </button>
+
+          <button className="header__button">
+            <FiShoppingBag className="header__button-icon" />
+            <span className="header__icon__cart-count">0</span>
+          </button>
+        </div>
+      </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam illum dolore temporibus magni nihil maiores debitis inventore qui a itaque necessitatibus eaque omnis, soluta pariatur vitae cupiditate, dolorum consequatur iste modi! Aut, sunt fugiat. Officia corporis nam eum assumenda hic nulla id blanditiis laborum quibusdam sequi, dolor, laudantium voluptas repellendus.</p>
+  );
+}
+
+export default Home;

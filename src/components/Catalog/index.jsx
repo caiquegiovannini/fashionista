a293ca14f5e3ca105db6a10a4489918cedef4b27
Caique <caique_giovannini@hotmail.com>
2020-06-13 12:59:13 -0300

    add panel

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -3,17 +3,27 @@ import { useSelector } from 'react-redux';
 
 import Card from '../Card';
 
+import Image from '../../assets/image-1.jpg';
+
 import './styles.css';
 
 const Catalog = () => {
   const { products } = useSelector(state => state.products);
 
   return (
-    <main className="catalog container">
-      {products &&
-        products.map(product => (<Card key={product.sizes[0].sku} product={product} />))
-      }
-    </main>
+    <div className="container">
+      <header className="panel">
+        <figure className="panel__image">
+          <img src={Image} alt="Imagem do painel"/>
+        </figure>
+      </header>
+
+      <main className="catalog">
+        {products &&
+          products.map(product => (<Card key={product.sizes[0].sku} product={product} />))
+        }
+      </main>
+    </div>
   );
 }
 
